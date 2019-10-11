1bd601de0277ca742806739515a47f583f964c48
Mark Hillard <markhillard@gmail.com>
2019-10-11 00:15:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+icanhazdadjoke
+==============
+
+Get some dad jokes from https://icanhazdadjoke.com/api
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/icanhazdadjoke.svg)](https://npmjs.org/package/icanhazdadjoke)
+[![Downloads/week](https://img.shields.io/npm/dw/icanhazdadjoke.svg)](https://npmjs.org/package/icanhazdadjoke)
+[![License](https://img.shields.io/npm/l/icanhazdadjoke.svg)](https://github.com/markhillard/icanhazdadjoke/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,69 @@
+// globals
+const { Command, flags } = require('@oclif/command'),
+  request = require('request'),
+  readline = require('readline'),
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  }),
+  prefix = '> '
+
+// init joke command
+class IcanhazdadjokeCommand extends Command {
+  // es2017 (es8) async function
+  async run() {
+    // exported flags
+    const { flags } = this.parse(IcanhazdadjokeCommand), // eslint-disable-line no-unused-vars
+      // api options
+      options = {
+        url: 'https://icanhazdadjoke.com/',
+        headers: {
+          Accept: 'text/plain'
+        }
+      },
+      // joke handler
+      joke = (error, response, body) => {
+        let message
+        try {
+          message = body.trim()
+        } catch (error) {
+          message = `error: ${error.stack}`
+        }
+        process.stdout.write('\n' + message + '\n\n' + prefix)
+      }
+
+    // show usage instructions
+    rl.setPrompt('Press enter for a joke / press q to quit: \n' + prefix)
+    rl.prompt()
+
+    // watch for readline events
+    await rl
+      .on('line', line => {
+        switch (line.trim()) {
+          case 'q':
+            rl.close()
+            break
+          default:
+            request(options, joke)
+            break
+        }
+      })
+      .on('close', () => {
+        process.exit(0)
+      })
+  }
+}
+
+// cli description
+IcanhazdadjokeCommand.description = `Stream dad jokes from https://icanhazdadjoke.com/`
+
+// cli flags
+IcanhazdadjokeCommand.flags = {
+  // add --version flag
+  version: flags.version({ char: 'v' }),
+  // add --help flag
+  help: flags.help({ char: 'h' })
+}
+
+// expose cli command as a module
+module.exports = IcanhazdadjokeCommand
